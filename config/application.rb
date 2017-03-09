f7cdb048477bca231bfb9f599ff6082e6ec4cd84
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-09 13:35:26 +0100

    init app

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CitiesOfMonsters
+  class Application < Rails::Application
+    console do
+      ActiveRecord::Base.connection
+    
+    end
+  end
+end
