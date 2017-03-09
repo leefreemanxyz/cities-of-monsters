cf60192437dad18a2bd566dbe4248c92b02e2281
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-09 15:38:21 +0100

    added about and contact pages, inserted header partial

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,5 +1,9 @@
 class MonstersController < ApplicationController
   def index
-    @monsters = Monster.all 
+    @monsters = Monster.all
+  end
+
+  def show
+    @monster = Monster.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "monsters#index"
-
+  get "contact" => "pages#contact"
+  get "about" => "pages#about"
   get "monsters" => "monsters#index"
+  get "monsters/:id" => "monsters#show", as: :monster
 end
