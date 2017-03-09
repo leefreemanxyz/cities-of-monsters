efebf2d0dd28f0ba0e5065edd403b8d48d69fd2b
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-09 19:08:39 +0100

    add monster validations

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,9 @@
 class Monster < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :health, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 100}
+  validates :image_url, presence: true
   def order_by_name
     order(:name)
   end
